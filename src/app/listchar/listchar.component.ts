59e33dd0a907000db534cc51d297f48f2d1b5d7d
Andrew Glines <aglines@gmail.com>
2017-07-24 10:47:57 -0700

    dynamic routing

--- src/app/listchar/listchar.component.ts
+++ src/app/listchar/listchar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Char } from '../char.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listchar',
@@ -8,9 +9,15 @@ import { Char } from '../char.model';
 })
 export class ListcharComponent {
 
+  constructor(private router: Router){}
+
   chars: Char[] = [
     new Char("Agent Cooper", "FBI agent assigned to investigate the murder of Laura Palmer", "0", 1),
     new Char("Laura Palmer", "Twin Peaks resident discovered dead in the first scene", "0", 2)
   ];
+
+  goToCharPage(clickedChar: Char) {
+    this.router.navigate(['chars', clickedChar.id]);
+  };
 
 }
